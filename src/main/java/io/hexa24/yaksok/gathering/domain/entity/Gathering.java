07feb63d7a12a19e5917d082c03f5fbdb96c4585
Reading-Snail <readingsnail17@gmail.com>
2024-02-14 21:49:23 +0900

    👔 Add: GathringEntity

--- src/main/java/io/hexa24/yaksok/gathering/domain/entity/Gathering.java
+++ src/main/java/io/hexa24/yaksok/gathering/domain/entity/Gathering.java
@@ -1,5 +1,20 @@
 package io.hexa24.yaksok.gathering.domain.entity;
 
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.geo.Point;
+
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Builder
 public class Gathering {
-    
+    @Id
+    private UUID id;
+    private String name;
+    private Point point; 
 }
