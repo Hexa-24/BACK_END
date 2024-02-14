3ace5921492c15d888825a7cba88f5411389f5e4
Reading-Snail <readingsnail17@gmail.com>
2024-02-14 21:35:02 +0900

    🔧 Add: @CompontScan by considering changed packege strcuture

--- src/main/java/io/hexa24/yaksok/YaksokApplication.java
+++ src/main/java/io/hexa24/yaksok/YaksokApplication.java
@@ -2,8 +2,10 @@ package io.hexa24.yaksok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"io.hexa24.global", "io.hexa24.yaksok"})
 public class YaksokApplication {
 
 	public static void main(String[] args) {
