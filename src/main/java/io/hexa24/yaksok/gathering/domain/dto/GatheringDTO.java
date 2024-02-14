f7120c31810219253ce2ff19ca8d633c8f94cc24
Reading-Snail <readingsnail17@gmail.com>
2024-02-14 22:34:49 +0900

    🏷️ Add: GathringDTO

--- src/main/java/io/hexa24/yaksok/gathering/domain/dto/GatheringDTO.java
+++ src/main/java/io/hexa24/yaksok/gathering/domain/dto/GatheringDTO.java
@@ -1,5 +1,16 @@
 package io.hexa24.yaksok.gathering.domain.dto;
 
+import java.util.UUID;
+
+import org.springframework.data.geo.Point;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class GatheringDTO {
-    
+    private UUID id;
+    private String name;
+    private Point point;     
 }
