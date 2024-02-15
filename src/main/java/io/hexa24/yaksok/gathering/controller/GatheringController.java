19f748df362fcc24ca66aaf6a8245a5e12fc75c8
Reading-Snail <readingsnail17@gmail.com>
2024-02-15 20:22:49 +0900

    Add: convertToDTO

--- src/main/java/io/hexa24/yaksok/gathering/controller/GatheringController.java
+++ src/main/java/io/hexa24/yaksok/gathering/controller/GatheringController.java
@@ -1,22 +1,19 @@
 package io.hexa24.yaksok.gathering.controller;
 
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.hexa24.yaksok.gathering.domain.dto.GatheringDTO;
 import io.hexa24.yaksok.gathering.domain.entity.Gathering;
 import io.hexa24.yaksok.gathering.service.GathringService;
-import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.Optional;
-import java.util.UUID;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("gatherings")
@@ -25,16 +22,19 @@ public class GatheringController {
 
     private final GathringService gathringService;
 
+    private final ModelMapper modelMapper;
+
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public GatheringDTO getGathering(@PathVariable UUID id) {
         Gathering gathering = gathringService.findGathering(id); // 이 코드가 꼭 필요할지는 고민, PathVariable에서 null 값이 들어올 수 있나?
-        GatheringDTO gatheringDTO = GatheringDTO.builder()
-                                                .id(gathering.getId())
-                                                .name(gathering.getName())
-                                                .point(gathering.getPoint())
-                                                .build();
+        GatheringDTO gatheringDTO = convertToDto(gathering);
         return gatheringDTO;
     }
     
+    private GatheringDTO convertToDto(Gathering gathering) {
+        GatheringDTO gatheringDTO = modelMapper.map(gathering, GatheringDTO.class);
+        return gatheringDTO;
+    }
+
 }
