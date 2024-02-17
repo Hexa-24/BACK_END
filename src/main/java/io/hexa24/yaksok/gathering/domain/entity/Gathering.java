08be410634e12eca76713435eacdefd21fbaf8fb
Reading-Snail <readingsnail17@gmail.com>
2024-02-17 21:36:51 +0900

    Modify: Gathring with Constructor

--- src/main/java/io/hexa24/yaksok/gathering/domain/entity/Gathering.java
+++ src/main/java/io/hexa24/yaksok/gathering/domain/entity/Gathering.java
@@ -8,14 +8,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @Getter
 @Builder
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Gathering {
     
     @Id
