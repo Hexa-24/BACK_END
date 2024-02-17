934a30e9f56a6bacac48c05bad8f79b2dead4d04
Reading-Snail <readingsnail17@gmail.com>
2024-02-17 22:53:39 +0900

    Add: RequestMapping for LocationController

--- src/main/java/io/hexa24/yaksok/location/controller/LocationController.java
+++ src/main/java/io/hexa24/yaksok/location/controller/LocationController.java
@@ -1,11 +1,13 @@
 package io.hexa24.yaksok.location.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.hexa24.yaksok.location.service.LocationServiceImpl;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("gatherings/{gatheringId}/members/{memberId}/locations")
 @RequiredArgsConstructor
 public class LocationController {
     
