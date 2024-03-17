f86c8833b300fbf61ef76efac4cc1d143a988085
Reading-Snail <readingsnail17@gmail.com>
2024-03-17 21:02:12 +0900

    Add: @Builder in Address.java

--- src/main/java/io/hexa24/yaksok/location/domain/value/Address.java
+++ src/main/java/io/hexa24/yaksok/location/domain/value/Address.java
@@ -1,12 +1,18 @@
 package io.hexa24.yaksok.location.domain.value;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 
     String zipCode;
     String address1;
     String address2;
-    
+
 }
