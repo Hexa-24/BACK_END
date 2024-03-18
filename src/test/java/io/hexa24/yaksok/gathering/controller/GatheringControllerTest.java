b2c1b79abc44ee5ca89982585af1df1c83a13df4
Reading-Snail <readingsnail17@gmail.com>
2024-03-18 14:41:58 +0900

    Add: @Transactional,@Rollback in MemberControllerTest

--- src/test/java/io/hexa24/yaksok/gathering/controller/GatheringControllerTest.java
+++ src/test/java/io/hexa24/yaksok/gathering/controller/GatheringControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +26,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = YaksokApplication.class)
 @AutoConfigureMockMvc
+@Transactional
+@Rollback
 @Slf4j
 class GatheringControllerTest {
 
@@ -43,7 +46,6 @@ class GatheringControllerTest {
      * @throws Exception
      */
     @Test
-    @Transactional
     void postGathering() throws Exception {
     // GIVEN: DTO 객체 생성
         GatheringPostReqDTO gatheringPostReqDTO = GatheringPostReqDTO.builder()
@@ -83,7 +85,6 @@ class GatheringControllerTest {
     }
 
     @Test
-    @Transactional
     void getGathering() throws Exception {
     // GIVEN
         Gathering gathering = Gathering.builder()
