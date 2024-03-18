32b31632b5a8ba1bc60417190749ef74d812e64d
Reading-Snail <readingsnail17@gmail.com>
2024-03-18 14:43:17 +0900

    Modify: MemberController::postMember with TDD

--- src/test/java/io/hexa24/yaksok/member/controller/MemberControllerTest.java
+++ src/test/java/io/hexa24/yaksok/member/controller/MemberControllerTest.java
@@ -56,23 +56,21 @@ class MemberControllerTest {
                 .name("4/24 모임 약속")
                 .build();
         Gathering gathering = gatheringRepository.save(gatheringDTO);
-
-
-    // WHEN: Mock으로 모임 정보를 생성하는 json 전달
+    // WHEN
+        // 모임 생성 정보 JSON으로 변환
         MemberReqDTO member1 = MemberReqDTO.builder()
                 .name("철수")
                 .build();
-
-        // 모임 생성 정보 JSON으로 변환
         Gson gson = new GsonBuilder().create();
         String jsonPayload = gson.toJson(member1);
+
+        // mockMvc
         mockMvc.perform(
             MockMvcRequestBuilders
                     .post("/gatherings/{gatheringId}/members",gathering.getId())
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(jsonPayload)
         )
-
     // THEN: Created(201) 상태코드 확인
             // location Header의 정보를 확인하고자 하는데 UUID를 DB에서 생성하여 대조할 방법이 없음
             //.andExpect(header().string("location","/gatherings/{gatheringId}/members"))
